cac3391ed3caa58c599bbc40f3574763ecbd4c66
Steven Kidd <xerofire@hotmail.co.uk>
2018-02-07 15:31:40 +0000

    convert message to toast

--- scripts/game/galaxy.js
+++ scripts/game/galaxy.js
@@ -8,14 +8,7 @@ function doit(missionID, planetID) {
 				$('#elementID'+elementID).text(number_format(value));
 			});
 		}
-		
-		var statustable	= $('#fleetstatusrow');
-		var messages	= statustable.find("~tr");
-		if(messages.length == MaxFleetSetting) {
-			messages.filter(':last').remove();
-		}
-		var element		= $('<td />').attr('colspan', 8).attr('class', data.code == 600 ? "success" : "error").text(data.mess).wrap('<tr />').parent();
-		statustable.removeAttr('style').after(element);
+		data.code == 600 ? toastr["success"](data.mess,"Fleet Dispatched") : toastr["error"](data.mess,"Error");
 	});
 }
 
