f6026a806d6a48b1437cd9e1d1bbc8648261233a
Steven Kidd <xerofire@hotmail.co.uk>
2018-02-18 20:34:52 +0000

    remove jquery ui autocomplete

--- scripts/game/base.js
+++ scripts/game/base.js
@@ -265,20 +265,6 @@ function _timeformat (seconds) {
     return ret;
 }
 
-$.widget("custom.catcomplete", $.ui.autocomplete, {
-	_renderMenu: function( ul, items ) {
-		var self = this,
-			currentCategory = "";
-		$.each( items, function( index, item ) {
-			if ( item.category != currentCategory ) {
-				ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
-				currentCategory = item.category;
-			}
-			self._renderItem( ul, item );
-		});
-	}
-});
-
 $(function() {
 	$('#drop-admin').on('click', function() {
 		$.get('admin.php?page=logout', function() {
